d76b1b7f846978e745923bc3cb438d4c5147646f
Alex Turkin <snowwlex@gmail.com>
2019-07-28 18:47:23 +0100

    make it ocmpatibly with scrapy >= 1.7

--- etsy/feedexport.py
+++ etsy/feedexport.py
@@ -4,12 +4,13 @@ CSV writer, resulting in EXPORT_FIELDS and EXPORT_ENCODING being ignored
 (EXPORT_EMPTY is not used by CSV).
 """
 
-from scrapy.conf import settings
+from scrapy.utils.project import get_project_settings
 from scrapy.exporters import CsvItemExporter
 
 class CSVkwItemExporter(CsvItemExporter):
 
     def __init__(self, *args, **kwargs):
+        settings = get_project_settings()
         kwargs['fields_to_export'] = settings.getlist('EXPORT_FIELDS') or None
         kwargs['encoding'] = settings.get('EXPORT_ENCODING', 'utf-8')
 
